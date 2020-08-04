c85a575f44edd173902117959272d64f3dc553a4
ehellas <ehellas@gmail.com>
2020-08-04 13:31:13 -0300

    quality of life changes

--- ModelStudio/imputation.r
+++ ModelStudio/imputation.r
@@ -1,17 +1,23 @@
 library('missForest')
 
+## Transformando espaco em branco em NA
 dm_traindf[dm_traindf==""] <- NA
 
+## Aplicando Inputacao
 imp <- missForest(dm_traindf, maxiter = 2, ntree = 30) 
 
+## retirando dados imputados
 dm_scoreddf <- imp$ximp
 dm_scoreddf$BAD <- as.numeric(dm_scoreddf$BAD) - 1
 
+## Output BAD
 print("/nInput")
 head(dm_traindf$BAD)
 
+## Data frame output
 print("/nScored")
 head(dm_scoreddf$BAD)
 
+## Data frame print
 print("/nDataFrame")
 head(dm_scoreddf)
